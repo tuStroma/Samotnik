0fbfe17f0933c0096b43f57965e9a43720c39e0a
Sławomir Stromski <j.iksmorts@gmail.com>
2022-10-09 20:45:54 +0200

    Game logic and result screen

--- Board.cs
+++ Board.cs
@@ -11,9 +11,56 @@ namespace WpfApp1
 {
     class Board
     {
+        private enum BoardCellType
+        {
+            FULL,
+            EMPTY,
+
+            NOT_BOARD
+        }
+
+        private class cell
+        {
+            public Board parent;
+            public int x, y;
+            public Button button = null;
+            public BoardCellType type;
+
+            public cell(Board parent, int x, int y)
+            {
+                this.parent = parent;
+                this.x = x;
+                this.y = y;
+            }
+
+            public void setType(BoardCellType type) 
+            {
+                this.type = type;
+                if (button != null)
+                {
+                    if (type == BoardCellType.FULL) button.Background = Brushes.Coral;
+                    else if (type == BoardCellType.EMPTY) button.Background = Brushes.Gray;
+                }
+            }
+
+            public void setButton(Button button) { this.button = button; }
+
+            public void clickAction(object sender, RoutedEventArgs e)
+            {
+                int result = parent.clickHandler(this);
+                if (result >= 0)
+                    parent.window.endScreen(result);
+            }
+        }
+
         private int width;
         private int height;
+
+        private cell[,] board;
+        private cell clicked = null;
+
         MainWindow window;
+        public Grid Grid;
 
         public Board(MainWindow window, int width, int height)
         {
@@ -21,7 +68,21 @@ namespace WpfApp1
             this.height = height;
             this.window = window;
 
-            int[,] cells = new int[width, height];
+            board = new cell[width, height];
+
+            for(int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    board[i, j] = new cell(this, i, j);
+                    if (!isBoard(i, j))
+                        board[i, j].setType(BoardCellType.NOT_BOARD);
+                    else if (isEmpty(i, j))
+                        board[i, j].setType(BoardCellType.EMPTY);
+                    else
+                        board[i, j].setType(BoardCellType.FULL);
+                }
+            }
 
 
             Grid grid = createGrid(width, height);
@@ -34,12 +95,11 @@ namespace WpfApp1
             int cell_width = 50;
             int cell_heigth = 50;
 
-            Grid Grid = new Grid();
+            Grid = new Grid();
             window.grid.Children.Add(Grid);
 
             Grid.Width = cell_width * x;
             Grid.Height = cell_heigth * y;
-            //Grid.ShowGridLines = true;
 
             // Create Columns
             for (int i = 0; i < x; i++)
@@ -64,9 +124,9 @@ namespace WpfApp1
         {
             for (int i = 0; i < x; ++i)
             {
-                for (int j = 0; j < 10; ++j)
+                for (int j = 0; j < y; ++j)
                 {
-                    if (!isBoard(i, j))
+                    if (board[i, j].type == BoardCellType.NOT_BOARD)
                         continue;
 
                     Button button = new Button()
@@ -77,13 +137,15 @@ namespace WpfApp1
                         Tag = i
                     };
 
-                    button.Background = isEmpty(i, j) ? Brushes.Gray : Brushes.Coral;
+                    button.Background = board[i, j].type == BoardCellType.EMPTY ? Brushes.Gray : Brushes.Coral;
 
-                    Grid.SetColumn(button, j);
-                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, i);
+                    Grid.SetRow(button, j);
                     grid.Children.Add(button);
 
-                    //button.Click += new RoutedEventHandler(button_Click);
+                    board[i, j].setButton(button);
+
+                    button.Click += new RoutedEventHandler(board[i, j].clickAction);
                 };
             };
         }
@@ -97,6 +159,106 @@ namespace WpfApp1
         private bool isEmpty(int x, int y)
         {
             return x == 3 && y == 3;
+        }
+
+        private int clickHandler(cell c)
+        {
+            int x = c.x;
+            int y = c.y;
+
+            switch (board[x, y].type)
+            {
+                case BoardCellType.FULL:
+                    {
+                        if (clicked != null) clicked.button.Background = Brushes.Coral;
+                        clicked = c;
+                        board[x, y].button.Background = Brushes.Red;
+                        break;
+                    }
+                case BoardCellType.EMPTY:
+                    {
+                        if (clicked != null)
+                        {
+                            int dx = c.x - clicked.x;
+                            int dy = c.y - clicked.y;
+
+                            if ((Math.Abs(dx) == 2 && dy == 0) ||
+                                (Math.Abs(dy) == 2 && dx == 0))
+                            {
+                                cell middle = board[(c.x + clicked.x) / 2, (c.y + clicked.y) / 2];
+                                if (middle.type == BoardCellType.FULL)
+                                {
+                                    c.setType(BoardCellType.FULL);
+                                    clicked.setType(BoardCellType.EMPTY);
+                                    middle.setType(BoardCellType.EMPTY);
+                                    clicked = null;
+                                    int result = checkResult();
+                                    return result;
+                                }
+                            }
+                        }
+                        break;
+                    }
+            }
+
+            return -1;
+        }
+
+        private int checkResult()
+        {
+            BoardCellType[] pattern = { BoardCellType.FULL,
+                                        BoardCellType.FULL,
+                                        BoardCellType.EMPTY };
+
+
+            for (int i = 0; i < width - pattern.Length + 1; i++)
+            {
+                for (int j = 0; j < height - pattern.Length + 1; j++)
+                {
+                    if (findHorisontal      (i, j, pattern)) return -1;
+                    if (findHorisontal_b    (i, j, pattern)) return -1;
+                    if (findVertical        (i, j, pattern)) return -1;
+                    if (findVertical_b      (i, j, pattern)) return -1;
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    if (board[i, j].type == BoardCellType.FULL)
+                        count++;
+
+            return count;
+        }
+
+
+        private bool findHorisontal(int x, int y, BoardCellType[] pattern)
+        {
+            for (int k = 0; k < pattern.Length; k++)
+                if (board[x + k, y].type != pattern[k])
+                    return false;
+            return true;
+        }
+        private bool findHorisontal_b(int x, int y, BoardCellType[] pattern)
+        {
+            for (int k = 0; k < pattern.Length; k++)
+                if (board[x + k, y].type != pattern[pattern.Length - k - 1])
+                    return false;
+            return true;
+        }
+        private bool findVertical(int x, int y, BoardCellType[] pattern)
+        {
+            for (int k = 0; k < pattern.Length; k++)
+                if (board[x, y + k].type != pattern[k])
+                    return false;
+            return true;
+        }
+        private bool findVertical_b(int x, int y, BoardCellType[] pattern)
+        {
+            for (int k = 0; k < pattern.Length; k++)
+                if (board[x, y + k].type != pattern[pattern.Length - k - 1])
+                    return false;
+            return true;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,11 +23,52 @@ namespace WpfApp1
         private int cols = 7;
         private int rows = 7;
 
+        Board board;
+        Grid result_screen_grid;
+
         public MainWindow()
         {
             InitializeComponent();
-            Board board = new Board(this, cols, rows);
+            board = new Board(this, cols, rows);
 
+        }
+
+        public void endScreen(int result)
+        {
+            this.grid.Children.Remove(board.Grid);
+
+            TextBlock msg = new TextBlock();
+            msg.Text = "Your result: " + result.ToString();
+            msg.VerticalAlignment = VerticalAlignment.Center;
+            msg.TextAlignment = TextAlignment.Center;
+
+            Button restart = new Button();
+            restart.Content = "Restart";
+            restart.Width = 100;
+            restart.Height = 50;
+            restart.Click += new RoutedEventHandler(RestartGame);
+
+
+            Grid grid = new Grid();
+            result_screen_grid = grid;
+            this.grid.Children.Add(grid);
+            for (int i = 0; i < 2; i++)
+            {
+                RowDefinition gridRow = new RowDefinition();
+                gridRow.Height = new GridLength(100);
+                grid.RowDefinitions.Add(gridRow);
+            }
+
+            Grid.SetRow(msg, 0);
+            grid.Children.Add(msg);
+            Grid.SetRow(restart, 1);
+            grid.Children.Add(restart);
+        }
+
+        private void RestartGame(object sender, RoutedEventArgs e)
+        {
+            this.grid.Children.Remove(result_screen_grid);
+            board = new Board(this, cols, rows);
         }
     }
 }
